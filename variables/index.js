a623bdb5fb5fb9a34cd4d7220305d911c107fe85
dotdwebo <dotdwebo@gmail.com>
2022-09-05 19:15:16 +0700

    Rename the function from function callx() to call(). Remove a parameter from the function. Console.log the variables and constants outside the function. Result: Success

--- variables/index.js
+++ variables/index.js
@@ -7,12 +7,15 @@ const z = 'This is a constant';
 //  z = 1; //error
 
 
- function callx(x){
-    var x = 1;
+ function call(){
+    // var x = 1;
+    
     console.log('I call var x! Wanna know the result. Result: '+x)
+    console.log('I call let y! Wanna know the result. Result: '+y)
+    console.log('I call const z! Wanna know the result. Result: '+z)
  }
 
-console.log(x);
-console.log(y);
-callx();
+// console.log(x);
+// console.log(y);
+call();
 // console.log(z); //error
