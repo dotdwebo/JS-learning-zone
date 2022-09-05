3b96fc9a7d75c1b77a5e2987612117cd9b66d9ad
dotdwebo <dotdwebo@gmail.com>
2022-09-05 18:56:15 +0700

    Assigned new value in var x, let y, and const z. Got error in cons z. However, it's a constant, right?

--- variables/index.js
+++ variables/index.js
@@ -2,6 +2,10 @@ var x = 'hello world';
 let y = 'hi hi hi';
 const z = 'This is a constant';
 
+ x = 99;
+ y = true;
+ z = 1;
+
 console.log(x);
 console.log(y);
-console.log(z);
+console.log(z); //error
