8f0803620fda2ae8525d76343dcafb7fe299db96
dotdwebo <dotdwebo@gmail.com>
2022-09-06 01:46:10 +0700

    result of variable hoisting: var -> undefined, let -> ReferenceError

--- variables/hoisting.js
+++ variables/hoisting.js
@@ -5,3 +5,10 @@ hi();
 function hi(){
     console.log('Hello World!');
 }
+
+// variable hoisting
+
+console.log(g); //undefined
+//let g // referrence error declaration
+var g ; //undefined
+g = 'Good Morning'; //initialization
