d7c5c3dd0cc8532b540c645fe0f91975761d36a0
dotdwebo <dotdwebo@gmail.com>
2022-09-05 20:11:34 +0700

    Only calling variable inside the function

--- variables/varvslet.js
+++ variables/varvslet.js
@@ -1,16 +1,20 @@
+let x = 'let outside';
+
+
 function test(){
 
-    let x = 'Using let';
+    let x = 'let Inside';
     console.log(x);
 }
 
 test();
 
 function test2(){
-    var x = 'Using var';
+    var x = 'var Inside';
     console.log(x);
 }
 
 test2();
 
-console.log(x); //ReferenceError: x is not defined.
+// console.log(x); //ReferenceError: x is not defined. | old: test.
+
