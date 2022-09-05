254d9007391f4d1dbdfc0892f31768a87340481a
dotdwebo <dotdwebo@gmail.com>
2022-09-05 23:18:06 +0700

    Created var and let inside function scope. Got an Reference Error on let.

--- variables/scope.js
+++ variables/scope.js
@@ -3,6 +3,14 @@ function hit(){
     let yoo = 'Yoo, I used let!';
 
     console.log(hey, yoo);
+
+    {
+        var wow = "Wow, I used var!";
+        let rawr = "Rawr, I am excited!";
+    }
+
+    console.log(wow);
+    console.log(rawr); //ReferrenceError
 }
 
 hit();
