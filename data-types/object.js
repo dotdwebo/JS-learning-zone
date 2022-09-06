d9dd10616f236863a678612e5003b2cd0531d6e7
dotdwebo <dotdwebo@gmail.com>
2022-09-06 23:53:51 +0700

    Create an object and array.

---
+++ data-types/object.js
@@ -0,0 +1,14 @@
+const obj = {
+    'name': 'Satoshi',
+    'age': 100,
+    'innovation': "Bitcoin"
+}
+
+console.log(typeof obj) //object
+console.log(obj.name); //Satoshi
+console.log(obj.age); //100
+
+const data = [1,2,3,4];
+
+console.log(typeof data); //object 
+console.log(data);
