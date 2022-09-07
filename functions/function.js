55f2df46346d65cb29e6d994cbd5bba7820f26a6
dotdwebo <dotdwebo@gmail.com>
2022-09-08 03:27:20 +0700

    Create function number() [#function]. Testing odd or even number.

--- functions/function.js
+++ functions/function.js
@@ -9,3 +9,16 @@ function hello(){
 }
 
 hello(); //hello world.
+
+function number(n){
+
+    if(n % 2 == 0){
+        console.log("It's an even number");
+    }else{
+        console.log("It's an odd number");
+    }
+}
+
+
+number(1); //It's an odd number
+number(2); //It's an even number
